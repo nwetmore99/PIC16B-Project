f01e1d905e84662d69d9d47f9944d8fc708df50a
ReetinavDas <reetinav@gmail.com>
2023-11-21 15:29:13 -0800

    changed camera.py to what updated_camera.py was and removed that, to make things less confusing

--- camera.py
+++ camera.py
@@ -2,14 +2,13 @@ import mediapipe as mp
 import cv2
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import pickle
 import time
 from gestures import *
-import numpy as np
 
-mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
-classes = ("down", "up", "thumbs up")
+classes = ("down", "up", "thumbs up", "stop")
 
 class HandNetwork(nn.Module):
     def __init__(self):
@@ -18,7 +17,7 @@ class HandNetwork(nn.Module):
         self.fc1 = nn.Linear(42, 120)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(120, 100)
-        self.fc3 = nn.Linear(100, 3)
+        self.fc3 = nn.Linear(100, len(classes))
     def forward(self, x):
         x = self.flatten(x)
         x = self.fc1(x)
@@ -28,38 +27,47 @@ class HandNetwork(nn.Module):
         x = self.fc3(x)
         return x
 
-with open("models/model2.pkl", "rb") as file:
+with open("models/model3.pkl", "rb") as file:
     model = pickle.load(file)
+
+model.eval()
 
 cap = cv2.VideoCapture(0)
 frame_counter = 0
 prev_time = 0
-with mp_hands.Hands(min_detection_confidence=0.95, min_tracking_confidence=0.7, max_num_hands=1) as hands:
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5, max_num_hands=1) as hands:
     while cap.isOpened():
-        ret, frame = cap.read()
-        landmarks = []
-        # Detections
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # changes from bgr to rgb since cv2 is bgr but mediapipe requires rgb
-        image.flags.writeable = False
-        results = hands.process(image) # this makes the actual detections
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
+        ret, image = cap.read()
         frame_counter += 1
+        landmarks = []
 
-        if results.multi_hand_landmarks:
-            for landmark in results.multi_hand_landmarks[0].landmark:
-                x, y = landmark.x, landmark.y
-                landmarks.append([x,y])
-            landmarks = np.array(landmarks)
-            out = model.predict(landmarks.reshape(-1,21*2)[0].reshape(1,-1)).item()
-            print(f"Prediction: {classes[out]}")
-            if classes[out] == 'up':
-                increase_volume()
-            if classes[out] == 'down':
-                decrease_volume()
-            if classes[out] == 'thumbs up':
-                skip_track()
+        if frame_counter % 2 == 0:
+            # Detections
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # changes from bgr to rgb since cv2 is bgr but mediapipe requires rgb
+            image.flags.writeable = False
+            results = hands.process(image) # this makes the actual detections
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            if results.multi_hand_landmarks:
+                for landmark in results.multi_hand_landmarks[0].landmark:
+                    x, y = landmark.x, landmark.y
+                    landmarks.append([x,y])
+                with torch.no_grad():
+                    landmarks = torch.tensor(landmarks)
+                    out = model(landmarks.view(-1,21,2))
+                    confidence = torch.max(F.softmax(out,1)).item()
+                    prediction = torch.argmax(out)
+                    print(classes[prediction], confidence)
+                    if confidence >= 0.95:
+                        if classes[prediction] == 'up':
+                            increase_volume()
+                        if classes[prediction] == 'down':
+                            decrease_volume()
+                        if classes[prediction] == 'stop':
+                            play_pause()
+                        if classes[prediction] == 'thumbs up':
+                            break
 
         # Print fps
         curr_time = time.time()
